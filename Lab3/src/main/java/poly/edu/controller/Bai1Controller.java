abc290a2683d476ce773d44f3ebbafcc827c26ec
rangsung00100 <tvu17709@gmail.com>
2025-10-04 11:42:45 +0700

    PS47480_TruHoangAnhVu_Lab3

---
+++ Lab3/src/main/java/poly/edu/controller/Bai1Controller.java
@@ -0,0 +1,21 @@
+package poly.edu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import poly.edu.entity.Staff;
+
+@Controller
+public class Bai1Controller {
+	@GetMapping("/staff/detail")
+	public String detail(Model model) {
+		Staff staff = Staff.builder()
+				.id("user@gmail.com")
+				.fullname("nguyễn văn user")
+				.level(2)
+				.build();
+				model.addAttribute("staff", staff);
+		return "bai1/staff";
+	}
+}
